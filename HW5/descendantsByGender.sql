67c7a9915ab9ddef1dbd8cd0967b4bdf6f42f2d3
Joseph Heenan <jheenan@greenkeytech.com>
2017-03-07 16:53:15 -0500

    Updated by gender

--- HW5/descendantsByGender.sql
+++ HW5/descendantsByGender.sql
@@ -5,9 +5,9 @@ with recursive Descendants(parent, child) as (
  select P.parent, Descendants.child
     from Parent_Of P
     JOIN Descendants on P.child = Descendants.parent)
-    select D.parent as ancestor, D.child as descendant
+    select D.parent as ancestor, P2.gender as descendantgender, COUNT(*)
     from Descendants D, Person P1, Person P2
     WHERE D.parent = P1.name
     AND D.child = P2.name
-    AND P1.gender = P2.gender
-    ORDER BY parent ASC;
+    GROUP BY ancestor, descendantgender
+    ORDER BY ancestor ASC
