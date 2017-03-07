2fbe8a674b412f1be6f2eba4a9a7da8c5e1ec184
Joseph Heenan <jheenan@greenkeytech.com>
2017-03-07 16:49:25 -0500

    Proper recursive query 1

--- HW5/recursiveQuery.sql
+++ HW5/recursiveQuery.sql
@@ -1,9 +1,13 @@
 with recursive Descendants(parent, child) as (
- select parent, child 
+ select parent, child
     FROM Parent_Of P
  UNION ALL
- select P.parent, Descendants.child 
+ select P.parent, Descendants.child
     from Parent_Of P
     JOIN Descendants on P.child = Descendants.parent)
-    select parent, child from Descendants
+    select D.parent as ancestor, D.child as descendant
+    from Descendants D, Person P1, Person P2
+    WHERE D.parent = P1.name
+    AND D.child = P2.name
+    AND P1.gender = P2.gender
     ORDER BY parent ASC;
